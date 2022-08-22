01d5ab2ff4f469c16b4441d6af0655ca5d55349c
Gwennael Jean <gwennael.jean@gmail.com>
2022-08-22 20:58:47 +0200

    feat(authentication): affichage du formulaire de connexion

--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -2,28 +2,32 @@
 
 namespace App\Controller\Security;
 
+use App\Entity\User;
+use App\Form\Security\ConnexionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/connexion', name: 'app_security_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthorizationCheckerInterface $authorizationChecker, AuthenticationUtils $authenticationUtils): Response
     {
-        if ($this->getUser()) {
+        if ($authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
             return $this->redirectToRoute('app_home');
         }
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+        $user = new User();
+
+        $form = $this->createForm(ConnexionType::class, $user, [
+            'authenticationUtils' => $authenticationUtils
+        ]);
 
         return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error
+            'form' => $form->createView(),
+            'error' => $authenticationUtils->getLastAuthenticationError(),
         ]);
     }
 
