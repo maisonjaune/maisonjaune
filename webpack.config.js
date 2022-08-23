e46e0a9855779ccb222bf4aa193916ee74286a7e
Gwennael Jean <gwennael.jean@gmail.com>
2022-08-23 13:16:22 +0200

    feat(js): suppression de stimulus

--- webpack.config.js
+++ webpack.config.js
@@ -26,9 +26,6 @@ Encore
     .addEntry('recaptcha', './assets/recaptcha.js')
     .addEntry('cookies', './assets/cookies.js')
 
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
-
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
