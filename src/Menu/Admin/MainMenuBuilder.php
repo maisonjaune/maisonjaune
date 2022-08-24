b0eb8e019b4946b988ae757e762ad85ffb3f3683
Gwennael Jean <gwennael.jean@gmail.com>
2022-08-24 18:01:41 +0200

    feat(menu): creation de menu de la topbar

--- src/Menu/Admin/MainMenuBuilder.php
+++ src/Menu/Admin/MainMenuBuilder.php
@@ -19,4 +19,49 @@ class MainMenuBuilder
 
         return $menu;
     }
+
+    public function createtopbarMenu(): ItemInterface
+    {
+        $menu = $this->factory->createItem('root', [
+            'childrenAttributes' => [
+                'class' => 'navbar-nav ml-auto'
+            ],
+        ]);
+
+        $action = $menu->addChild('Action', [
+            'uri' => '#',
+            'childrenAttributes' => [
+                'class' => 'dropdown-menu dropdown-menu-end'
+            ],
+            'attributes' => [
+                'class' => 'nav-item dropdown'
+            ],
+            'linkAttributes' => [
+                'class' => 'nav-link dropdown-toggle',
+                'id' => 'action-dropdown',
+                'role' => 'button',
+                'data-bs-toggle' => 'dropdown',
+                'aria-expanded' => 'false'
+            ]
+        ]);
+
+
+        $action->addChild('Profile', [
+            'route' => 'app_security_logout',
+            'linkAttributes' => ['class' => 'dropdown-item']
+        ]);
+
+        $action->addChild('divider', [
+            'attributes' => [
+                'class' => 'dropdown-divider'
+            ],
+        ]);
+
+        $action->addChild('Logout', [
+            'route' => 'app_security_logout',
+            'linkAttributes' => ['class' => 'dropdown-item']
+        ]);
+
+        return $menu;
+    }
 }
