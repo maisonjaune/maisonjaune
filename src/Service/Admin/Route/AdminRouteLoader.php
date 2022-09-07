a7fd8744aa56b60bd3fd7228c2e24559b2708d74
Gwennael Jean <gwennael.jean@gmail.com>
2022-09-07 21:09:44 +0200

    feat(admin crud): création du AdminRouteLoader

---
+++ src/Service/Admin/Route/AdminRouteLoader.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Service\Admin\Route;
+
+use App\Service\Admin\AdminCRUDInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Config\Loader\Loader;
+use Symfony\Component\Routing\RouteCollection;
+
+class AdminRouteLoader extends Loader
+{
+    private Collection $admins;
+
+    private const ROUTE_TYPE = 'admin';
+
+    public function __construct(string $env = null)
+    {
+        parent::__construct($env);
+        $this->admins = new ArrayCollection();
+    }
+
+    public function addAdminCrud(AdminCRUDInterface $admin)
+    {
+        $this->admins->add($admin);
+    }
+
+    public function load(mixed $resource, string $type = null)
+    {
+        $routes = new RouteCollection();
+
+        foreach ($this->admins as $admin) {
+            $routes->addCollection($admin->getRouteCollection());
+        }
+
+        return $routes;
+    }
+
+    public function supports(mixed $resource, string $type = null)
+    {
+        return self::ROUTE_TYPE === $type;
+    }
+}
